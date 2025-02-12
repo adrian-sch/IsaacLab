debfee3ceb0055503cbfcfdb4a6d05204ede6eca
Adrian Schmelter (Isaac Workstation) <adrian.schmelter@iml.fraunhofer.de>
2025-02-12 21:48:12 +0100

    added noise to lidar cfg

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/robomaster/robomaster_env.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/robomaster/robomaster_env.py
@@ -235,7 +235,7 @@ class RobomasterEnv(DirectRLEnv):
                 ],
                 dim=-1,
             )
-
+            
         observations = {"policy": obs}
         return observations
 
